e39c03361dac9e4d658b8048d2a35a34d39bd8db
Daniel Garcia <pablo.garcia@neoris.com>
2022-04-05 12:35:03 -0500

    chore: add translate

---
+++ src/app/shared/translator/sharedTranslator.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, HttpClientModule, TranslateModule],
+  exports: [TranslateModule],
+})
+export class SharedTranslatoModule {}
---
+++ src/app/shared/translator/translator.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, '../../../assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'Es',
+    }),
+  ],
+  exports: [TranslateModule],
+})
+export class TranslatorModule {}

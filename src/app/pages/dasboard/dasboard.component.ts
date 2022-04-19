4853560b1959608787e4e0d9d04ffc8b8fec7a2d
Daniel Garcia <pablo.garcia@neoris.com>
2022-04-19 15:17:28 -0500

    feat: add sidenav and navbar

--- src/app/pages/dasboard/dasboard.component.ts
+++ src/app/pages/dasboard/dasboard.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dasboard',
   templateUrl: './dasboard.component.html',
-  styleUrls: ['./dasboard.component.scss']
+  styleUrls: ['./dasboard.component.scss'],
 })
 export class DasboardComponent implements OnInit {
+  rtl = false;
+  variant = 'positive';
+  userName = 'Example user';
+  title = 'app';
+  width = '80px';
+  termsRoute = '';
+  termsPolicy = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

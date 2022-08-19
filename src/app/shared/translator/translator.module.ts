bfaf956d5b0c228dbfe6656c5e91684d5b37061e
Daniel Garcia <pablo.garcia@neoris.com>
2022-08-19 14:19:25 -0500

    feat(eslint): rule is added to validate what a function returns

--- src/app/shared/translator/translator.module.ts
+++ src/app/shared/translator/translator.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-export function createTranslateLoader(http: HttpClient) {
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, '../../../assets/i18n/', '.json');
 }
 
